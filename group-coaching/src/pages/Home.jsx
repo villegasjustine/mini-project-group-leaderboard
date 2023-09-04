ed543fc8d6ac5656d9986723137f9d56d4a97d2c
villegasjustine <villegasjustine@gmail.com>
2023-09-04 17:14:40 +1200

    branch

--- group-coaching/src/pages/Home.jsx
+++ group-coaching/src/pages/Home.jsx
@@ -2,16 +2,17 @@ import Exercises from "../components/ExerciseBox";
 import Leaderboard from "../components/Leaderboard";
 import { useState } from "react";
 import { users } from "../data/users";
+import { useUserContext } from "../context/UserContext";
 
 export default function Home() {
   const [currentUser, setCurrentUser] = useState(users)
-
+  
     return (
       <div className="Home">
         Dashboard Page
       <ul>
         <Leaderboard users={currentUser}/>
-        <li>Welcome User</li>
+        <li>Welcome {currentUser.name}</li>
         <Exercises users={currentUser} setUsers={setCurrentUser}/>
         <li>Motivational Quote</li>
         <li>Video</li>
