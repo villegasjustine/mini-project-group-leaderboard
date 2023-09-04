3e79d5db379b9022531fb7f126fcdbddab296dcc
villegasjustine <villegasjustine@gmail.com>
2023-09-04 13:24:45 +1200

    signin

--- group-coaching/src/components/SignIn.jsx
+++ group-coaching/src/components/SignIn.jsx
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
 import { users } from '../data/users';
+import { useUserContext } from "../context/UserContext";
 
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 export default function SignIn() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [submitResult, setSubmitResult] = useState("");
+  const [loginAttempts, setLoginAttempts] = useState(0);
   const [errorMessage, setErrorMessage] = useState('');
 
+  const {currentUser, handleUpdateUser} = useUserContext();
 
-  const handleLogin = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     // Find the user with the entered username
@@ -16,10 +51,10 @@ export default function SignIn() {
 
     if (user) {
       if (password === 'password') {
-        // Successfully logged in
+        // successfully logged in
         setErrorMessage('');
         alert(`Welcome, ${user.name}! You are logged in as a ${user.type}.`);
-        // make user go to homepage
+        <NavLink to="/home">Home</NavLink>
       } else {
         setErrorMessage('Incorrect password');
       }
@@ -28,33 +63,89 @@ export default function SignIn() {
     }
   };
 
+  if (currentUser.name)
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-      </form>
-    </div>
+    <>
+      <p>Welcome {currentUser.email}!</p>
+      <button onClick={() => handleUpdateUser({})}>Log Out</button>
+    </>
   );
+else if (loginAttempts >= 5) return <p>Login attempts exceeded</p>;
+
+return (
+  
+  <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href="#" variant="body2">
+              Forgot password?
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="#" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+    <Copyright sx={{ mt: 8, mb: 4 }} />
+  </Container>
+
+);
 }
 
 
